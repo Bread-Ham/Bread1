aa2a62c31f9bc022f1d4f4c424b42cf614cf1920
Syn33d <128500319+Syn33d@users.noreply.github.com>
2024-10-18 14:57:42 +0200

    Add axios and building login page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState('')
+
+
+  const handleClick = async () => {
+    //appel a ham puis je reçois une url
+      const response = await axios.get('http://localhost:4000/login');
+      const data = response.data.json();
+      setUrl(data.url);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleClick}>Me connecter avec HAM</button>
+      {url && <p>Received URL: <a href={url} target="_blank" rel="noopener noreferrer">{url}</a></p>}
     </>
   )
 }
